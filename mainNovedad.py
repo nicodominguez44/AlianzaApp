e3911cb11bfaea6bad796b04d3f09e689005d1d9
nicodominguez44 <1000ideas.design@gmail.com>
2025-03-29 20:11:01 -0300

    Update mainNovedad.py

--- mainNovedad.py
+++ mainNovedad.py
@@ -21,7 +21,7 @@ def main_Novedad(page: ft.Page):
     page.window.width=360
     page.window.height=720
     page.horizontal_alignment=ft.CrossAxisAlignment.CENTER
-    page.padding= ft.padding.only(left=30, top=10, right=30, bottom=30)
+    page.padding= ft.padding.only(left=30, top=20, right=30, bottom=30)
 
 
     logo = ft.Image(src= 'logoFrateAlianza1.png',width=70,)
