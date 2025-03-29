73b2316a11f383e00c84502e983d2ce109690f12
nicodominguez44 <1000ideas.design@gmail.com>
2025-03-29 20:53:24 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,18 +2,19 @@ import flet as ft
 import mainlogin
 import mainInicio
 
-
 def main(page: ft.Page):
-    page.title: "controlador"
+    page.title = "Controlador"
 
     def show_mainlogin(e):
         page.clean()
         mainlogin.main_login(page)
 
-    usuario_sesion = mainlogin.main_login(page)
+    # Verificar si el usuario tiene una sesión iniciada
+    username = page.client_storage.get("username")
+    user_info = page.client_storage.get("user_info")
 
-    if usuario_sesion:
-        mainInicio.main_Inicio(page,page.client_storage.get("username"), page.client_storage.get("user_info"))
+    if username and user_info:  # Si hay username y user_info en client_storage, se asume sesión iniciada
+        mainInicio.main_Inicio(page, username, user_info)
     else:
         show_mainlogin(None)
 
