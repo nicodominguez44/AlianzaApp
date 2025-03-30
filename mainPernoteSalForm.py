67112e5a3c15ee180eabcd95959f40713458e93b
nicodominguez44 <1000ideas.design@gmail.com>
2025-03-30 16:53:28 -0300

    Update mainPernoteSalForm.py

--- mainPernoteSalForm.py
+++ mainPernoteSalForm.py
@@ -89,7 +89,7 @@ async def registrar_salida(page):
             page.update()
 
             # Llamar a la siguiente pantalla después de completar la operación
-            mainPernoteFinal.mainPernote_Final(page)
+            await mainPernoteFinal.mainPernote_Final_Inicio(page)
             return True
         except requests.exceptions.RequestException as e:
             print(f"Error al eliminar la entrada: {e}")
@@ -160,7 +160,8 @@ def limpiar_registro():
         os.remove(ARCHIVO_DATOS_ENTRADA)
 
 
-
+#####################################################################################################
+#PAGE1
 
 def mainPernote_SalForm(page: ft.Page):
     page.clean()
@@ -219,6 +220,7 @@ def mainPernote_SalForm(page: ft.Page):
 
         page.add(nombreyapellido)
         page.add(ft.ElevatedButton("Actualizar Nombre", on_click=actualizar_nombre_salida))
+        
     
             # SE FUE EN TREN O REMIS
 
@@ -228,7 +230,7 @@ def mainPernote_SalForm(page: ft.Page):
         globals.writed_train_sal = tf_tren_remis_salida.value
         page.update()
 
-    tf_tren_remis_salida=ft.TextField(
+    tf_tren_remis_salida=ft.TextField(value=globals.writed_train_sal,
                 bgcolor=ft.Colors.WHITE, color= ft.Colors.BLACK,
                 border_radius=ft.border_radius.only(top_left=0,top_right=15,bottom_left=15,bottom_right=15),
                 border_color=ft.Colors.CYAN_ACCENT, on_change= actualizar_tren
@@ -244,7 +246,7 @@ def mainPernote_SalForm(page: ft.Page):
         globals.writed_obs_sal = tf_observaciones_salida.value
         page.update()
 
-    tf_observaciones_salida=ft.TextField(
+    tf_observaciones_salida=ft.TextField( value=globals.writed_obs_sal,
                 bgcolor=ft.Colors.WHITE, text_size=14, color=ft.Colors.BLACK,
                 border_radius=ft.border_radius.only(top_left=0,top_right=15,bottom_left=15,bottom_right=15),
                 border_color=ft.Colors.CYAN_ACCENT, multiline=True, max_length=70, on_change=actualizar_obs
@@ -254,7 +256,8 @@ def mainPernote_SalForm(page: ft.Page):
 
             # FECHA DE SALIDA
 
-    selected_date_text=ft.Text("Sin seleccionar",color= ft.Colors.WHITE, style=ft.TextStyle(size=14,weight=ft.FontWeight.BOLD))
+    selected_date_text_value = globals.selected_date_sal if globals.selected_date_sal != "Sin seleccionar" else "Sin seleecionar"
+    selected_date_text=ft.Text(selected_date_text_value,color= ft.Colors.WHITE, style=ft.TextStyle(size=14,weight=ft.FontWeight.BOLD))
 
     def handle_change(e):
         # Actualizar el texto con la nueva fecha seleccionada
@@ -287,7 +290,8 @@ def mainPernote_SalForm(page: ft.Page):
             # HORA SALIDA
 
     # Crear un texto para mostrar la hora seleccionada
-    time_text = ft.Text("Sin seleccionar",color= ft.Colors.WHITE, style=ft.TextStyle(size=14,weight=ft.FontWeight.BOLD))
+    selected_time_text_value = globals.selected_time_sal if globals.selected_time_sal != "Sin seleccionar" else "Sin seleecionar"
+    time_text = ft.Text(selected_time_text_value,color= ft.Colors.WHITE, style=ft.TextStyle(size=14,weight=ft.FontWeight.BOLD))
 
     def handle_change1(e):
         # Actualiza el texto con la hora seleccionada
@@ -328,11 +332,9 @@ def mainPernote_SalForm(page: ft.Page):
     #Distribucion de la informacion
 
 
-    cambiar_f1= accionBotonPern.obtener_cambiar_f1()
-
-    async def async_registrar_salida_y_cambiar_f1(e, page):
-        if await registrar_salida(page):
-            cambiar_f1(e)
+    async def async_cambiar_pagina_final(e):
+        mainPernote_Final(page)
+            
 
 
     container= ft.Container(
@@ -348,11 +350,11 @@ def mainPernote_SalForm(page: ft.Page):
             ft.Row([pernocte_columna], alignment=ft.MainAxisAlignment.CENTER),
             RowFechaEnt,RowhoraEnt, ft.Row([columna_tren]),
             ft.Column([observaciones_columna]),           
-            ft.Row([ft.ElevatedButton(text="Registrar Salida",
+            ft.Row([ft.ElevatedButton(text="Ver Resumen",
                                       style= ft.ButtonStyle(
                                           text_style=ft.TextStyle(size=20,weight=ft.FontWeight.BOLD),color=ft.Colors.WHITE),
-                                     expand=True,height=60,bgcolor=ft.Colors.GREEN_ACCENT_400, color= ft.Colors.WHITE,
-                                     on_click=lambda e: asyncio.run(async_registrar_salida_y_cambiar_f1(e, page)) if validar_campos_sal(page) else None)
+                                     expand=True,height=50,bgcolor=ft.Colors.BLUE_ACCENT, color= ft.Colors.WHITE,
+                                     on_click=lambda e: asyncio.run(async_cambiar_pagina_final(e)) if validar_campos_sal(page) else None)
                     ]) 
             ], spacing=15
         )
@@ -385,3 +387,148 @@ def mainPernote_SalForm(page: ft.Page):
     )
 
     page.add(columna3)
+
+
+#####################################################################################################
+#PAGE2
+
+def mainPernote_Final(page: ft.Page):
+    page.clean()
+    page.title= "Tu entrada y salida"
+
+    page.decoration = ft.BoxDecoration(
+        gradient= ft.LinearGradient(
+            colors=[ft.Colors.BLUE_600, ft.Colors.INDIGO_900],
+            stops=[0,1],
+            begin=ft.alignment.top_center,
+            end=ft.alignment.bottom_center,
+        )
+    )
+
+    page.bgcolor= ft.Colors.TRANSPARENT
+    page.window.width=360
+    page.window.height=720
+    page.horizontal_alignment=ft.CrossAxisAlignment.CENTER
+    page.padding= ft.padding.only(left=30, top=50, right=30, bottom=30)
+
+    app_bar = ft.AppBar(
+        bgcolor= ft.Colors.TRANSPARENT,
+        leading=ft.IconButton(icon=ft.Icons.ARROW_CIRCLE_LEFT_SHARP,
+                              icon_size=40,icon_color=ft.Colors.BLUE_900,
+                              on_click=lambda e: mainPernote_SalForm(page))
+    )
+
+    page.add(app_bar)
+
+
+    logo = ft.Image(src= 'logoFrateAlianza1.png',width=70,)
+    titulo= ft.Text("Pernoctes", color= ft.Colors.WHITE, style=ft.TextStyle(size=20,weight=ft.FontWeight.BOLD))
+
+       #Textfields de formulario
+           # PERNOCTE TOMADO DE ENTRADA
+
+    pernocte_title=ft.Text("Pernocte",color= ft.Colors.WHITE, style=ft.TextStyle(size=10))
+    pernocte_value= globals.selected_option_pernocte
+    pernocte= ft.Text(pernocte_value, color= ft.Colors.CYAN_ACCENT, style=ft.TextStyle(size=18))
+    pernocte_columna= ft.Column([pernocte_title,pernocte],
+                                spacing=3,horizontal_alignment=ft.CrossAxisAlignment.CENTER)
+
+            # NOMBRE Y APELLIDO TOMADO DE ENTRADA
+    
+    nombreyapellido_title= ft.Text("Nombre y Apellido",color= ft.Colors.WHITE, style=ft.TextStyle(size=10))
+    nombreyapellido=ft.Text(globals.writed_name, color= ft.Colors.CYAN_ACCENT, style=ft.TextStyle(size=18))
+    nombreyapellido_columna= ft.Column([nombreyapellido_title,nombreyapellido],
+                                      spacing=3,horizontal_alignment=ft.CrossAxisAlignment.CENTER)
+    
+            # TEXTO ENTRADA SALIDA
+
+    registro_entrada_salida= ft.Text("Tu entrada y salida",
+                                      color= ft.Colors.CYAN_ACCENT, style=ft.TextStyle(size=24,weight=ft.FontWeight.W_300),
+                                      expand=True, text_align=ft.TextAlign.CENTER,)
+    row_registro_entr_sal= ft.Row([registro_entrada_salida])
+
+            # CONTEINER DEL REGISTRO
+    
+    conteiner_reg= ft.Container(
+        bgcolor=ft.Colors.BLUE_700,
+        alignment=ft.alignment.center,
+        border_radius= ft.border_radius.only(top_left=20, top_right=20,bottom_left=20, bottom_right=20),
+        expand= True,
+        height=300,
+        padding=ft.padding.only(left=10,top=10,right=10,bottom=10),
+        content=(ft.Column([
+            ft.Row([pernocte_columna], alignment=ft.MainAxisAlignment.CENTER),
+            ft.Row([nombreyapellido_columna],alignment=ft.MainAxisAlignment.CENTER),
+            ft.Row([ft.Text("ENTRADA",text_align=ft.TextAlign.CENTER,
+                            color= ft.Colors.WHITE, style=ft.TextStyle(size=18,weight=ft.FontWeight.BOLD)),
+                    ft.Text("________",color=ft.Colors.TRANSPARENT),
+                    ft.Text("SALIDA",text_align=ft.TextAlign.CENTER,
+                            color= ft.Colors.WHITE, style=ft.TextStyle(size=18,weight=ft.FontWeight.BOLD))],
+                   alignment=ft.MainAxisAlignment.CENTER),
+            ft.Row([ft.Text(globals.selected_date_ent,
+                            text_align=ft.TextAlign.CENTER,color= ft.Colors.CYAN_ACCENT),
+                    ft.Text("FECHA",text_align=ft.TextAlign.CENTER,
+                            color= ft.Colors.WHITE, style=ft.TextStyle(size=10,weight=ft.FontWeight.BOLD)),
+                    ft.Text(globals.selected_date_sal,text_align=ft.TextAlign.CENTER,color= ft.Colors.CYAN_ACCENT)],
+                    alignment=ft.MainAxisAlignment.CENTER),
+            ft.Row([ft.Text(globals.selected_time_ent,
+                            text_align=ft.TextAlign.CENTER,color= ft.Colors.CYAN_ACCENT),
+                    ft.Text("HORA",text_align=ft.TextAlign.CENTER,
+                            color= ft.Colors.WHITE, style=ft.TextStyle(size=10,weight=ft.FontWeight.BOLD)),
+                    ft.Text(globals.selected_time_sal,
+                            text_align=ft.TextAlign.CENTER,color= ft.Colors.CYAN_ACCENT)],
+                   alignment=ft.MainAxisAlignment.CENTER),
+            ft.Row([ft.Text(globals.writed_train_ent, color= ft.Colors.CYAN_ACCENT),
+                    ft.Text("TREN",text_align=ft.TextAlign.CENTER,
+                            color= ft.Colors.WHITE, style=ft.TextStyle(size=10,weight=ft.FontWeight.BOLD)),
+                    ft.Text(globals.writed_train_sal, color= ft.Colors.CYAN_ACCENT)],
+                   alignment=ft.MainAxisAlignment.CENTER),
+            ft.Column([ ft.Text("OBSERVACIONES",color= ft.Colors.WHITE,
+                                 style=ft.TextStyle(size=8,weight=ft.FontWeight.BOLD)),
+                    ft.Text(globals.writed_obs_ent,color= ft.Colors.CYAN_ACCENT),
+                    ft.Text(globals.writed_obs_sal, color= ft.Colors.CYAN_ACCENT)],
+                   alignment=ft.MainAxisAlignment.CENTER,horizontal_alignment=ft.CrossAxisAlignment.CENTER,spacing=5),
+        ], spacing=5,expand=True,horizontal_alignment=ft.CrossAxisAlignment.CENTER))
+    )
+
+    cambiar_f1= accionBotonPern.obtener_cambiar_f1()
+
+    async def async_registrar_salida_y_cambiar_f1(e, page):
+        if await registrar_salida(page):
+            cambiar_f1(e)
+
+    row11= ft.Row(
+        [
+            ft.ElevatedButton(
+            text="Registrar e Ir al inicio",
+            style= ft.ButtonStyle(
+                text_style=ft.TextStyle(size=20,weight=ft.FontWeight.BOLD),color=ft.Colors.WHITE),
+            expand=True,height=60, bgcolor=ft.Colors.GREEN_ACCENT_400,
+            on_click=lambda e: asyncio.run(async_registrar_salida_y_cambiar_f1(e, page)) if validar_campos_sal(page) else None)
+        ]
+    )
+
+    columna1= ft.Column(
+        controls=[logo,titulo,row_registro_entr_sal],
+        alignment=ft.MainAxisAlignment.CENTER,
+        horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+        spacing=7    
+    )
+
+    columna2= ft.Column(
+        controls=[conteiner_reg,row11],
+        alignment=ft.MainAxisAlignment.CENTER,
+        horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+        spacing=20
+    )
+
+    columna3= ft.Column(
+        controls=[columna1,columna2],
+        alignment=ft.MainAxisAlignment.CENTER,
+        horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+        spacing=20
+    )
+   
+    page.add(columna3)
+
+#ft.app(target=mainPernote_SalForm)
